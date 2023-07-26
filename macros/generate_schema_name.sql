f7861d04747e1cc92f93ca38765bcd03d7cbf845
sarah-tuva <sarah@tuvahealth.com>
2023-07-26 11:42:34 -0600

    Fix bug in schema macro that was creating tables under "none"

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -4,7 +4,10 @@
 #}
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
-    {{ custom_schema_name }}
-
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is not none -%}
+        {{ custom_schema_name | trim }}
+    {%- else -%}
+        {{ default_schema }}
+    {%- endif -%}
 {%- endmacro %}
